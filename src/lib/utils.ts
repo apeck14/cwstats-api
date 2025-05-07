f19e7ba6b684610e4b1ae30db38f262fbadb1cad
Aaron Peck <aaronpeck_14@yahoo.com>
2025-05-07 11:42:54 -0700

    fix: remove hyphen from league

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,17 +2,17 @@ import badges from '../static/badges.json'
 
 export const getLeague = (trophyCount: number): string => {
   const leagues = [
-    { min: 5000, name: 'legendary-3' },
-    { min: 4000, name: 'legendary-2' },
-    { min: 3000, name: 'legendary-1' },
-    { min: 2500, name: 'gold-3' },
-    { min: 2000, name: 'gold-2' },
-    { min: 1500, name: 'gold-1' },
-    { min: 1200, name: 'silver-3' },
-    { min: 900, name: 'silver-2' },
-    { min: 600, name: 'silver-1' },
-    { min: 400, name: 'bronze-3' },
-    { min: 200, name: 'bronze-2' },
+    { min: 5000, name: 'legendary3' },
+    { min: 4000, name: 'legendary2' },
+    { min: 3000, name: 'legendary1' },
+    { min: 2500, name: 'gold3' },
+    { min: 2000, name: 'gold2' },
+    { min: 1500, name: 'gold1' },
+    { min: 1200, name: 'silver3' },
+    { min: 900, name: 'silver2' },
+    { min: 600, name: 'silver1' },
+    { min: 400, name: 'bronze3' },
+    { min: 200, name: 'bronze2' },
   ]
 
   const found = leagues.find(({ min }) => trophyCount >= min)
