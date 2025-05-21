d8118c9fae981115bcae737b6aed3d101d72ec1a
Aaron Peck <aaronpeck_14@yahoo.com>
2025-05-20 17:32:30 -0700

    feat: add statistics model

---
+++ src/models/statistics.model.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, Model, Schema } from 'mongoose'
+
+interface Location {
+  id: number
+  name: string
+  isCountry: boolean
+  countryCode: string
+}
+
+interface ClanMovement {
+  tag: string
+  name: string
+  rank: number
+  previousRank: number
+  location: Location
+  clanScore: number
+  members: number
+  badgeId: number
+}
+
+interface Statistics extends Document {
+  lbLastUpdated: number
+  fallers: ClanMovement[]
+  risers: ClanMovement[]
+}
+
+const locationSchema = new Schema({
+  countryCode: { required: true, type: String },
+  id: { required: true, type: Number },
+  isCountry: { required: true, type: Boolean },
+  name: { required: true, type: String },
+})
+
+const clanMovementSchema = new Schema({
+  badgeId: { required: true, type: Number },
+  clanScore: { required: true, type: Number },
+  location: { required: true, type: locationSchema },
+  members: { required: true, type: Number },
+  name: { required: true, type: String },
+  previousRank: { required: true, type: Number },
+  rank: { required: true, type: Number },
+  tag: { required: true, type: String },
+})
+
+const StatisticsSchema = new Schema<Statistics>(
+  {
+    fallers: { required: true, type: [clanMovementSchema] },
+    lbLastUpdated: { required: true, type: Number },
+    risers: { required: true, type: [clanMovementSchema] },
+  },
+  { collection: 'Statistics' },
+)
+
+export const StaticsModel: Model<Statistics> =
+  mongoose.models.Statistics || mongoose.model<Statistics>('Statistics', StatisticsSchema)
