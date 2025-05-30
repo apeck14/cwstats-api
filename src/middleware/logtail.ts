ae25bc9dfe05442f076ad15553bd434c79c9465f
Aaron Peck <aaronpeck_14@yahoo.com>
2025-05-30 15:00:55 -0700

    chore: add isDev to logs

--- src/middleware/logtail.ts
+++ src/middleware/logtail.ts
@@ -6,6 +6,7 @@ export default function requestLogger(req: Request, res: Response, next: NextFun
   res.on('finish', () => {
     logger.info('HTTP Request', {
       ip: req.ip,
+      isDev: process.env.NODE_ENV === 'development',
       method: req.method,
       path: req.originalUrl,
       status: res.statusCode,
