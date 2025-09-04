540949d2d17392eb32df29f313850e50c009d69a
Aaron Peck <aaronpeck_14@yahoo.com>
2025-09-03 21:37:14 -0700

    fix: properly get active subscriptions

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,9 +1,10 @@
+import { formatTag } from '@/lib/format'
 import stripe from '@/lib/stripe'
 
 export const hasActiveSubscription = async (clanTag: string): Promise<boolean> => {
   try {
     const subscriptions = await stripe.subscriptions.search({
-      query: `metadata['clanTag']:'${clanTag}' AND (status:'active' OR status:'trialing')`,
+      query: `metadata['clanTag']:'${formatTag(clanTag, true)}' AND (status:'active' OR status:'trialing')`,
     })
     return subscriptions.data.length > 0
   } catch (error) {
