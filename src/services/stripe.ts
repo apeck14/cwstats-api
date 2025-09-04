ff9232373aefc3acff1039aa911e982f9bf81d94
Aaron Peck <aaronpeck_14@yahoo.com>
2025-09-03 21:10:51 -0700

    fix: include trialing clans

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,7 +3,7 @@ import stripe from '@/lib/stripe'
 export const hasActiveSubscription = async (clanTag: string): Promise<boolean> => {
   try {
     const subscriptions = await stripe.subscriptions.search({
-      query: `metadata['clanTag']:'${clanTag}' AND status:'active'`,
+      query: `metadata['clanTag']:'${clanTag}' AND (status:'active' OR status:'trialing')`,
     })
     return subscriptions.data.length > 0
   } catch (error) {
